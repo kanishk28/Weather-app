28fcd36106d83b15cdd642a9361fd5085fe95090
kanishk narain tyagi <kanishknaraintyagi@kanishks-MacBook-Air.local>
2023-08-29 18:28:10 +0530

    Completed the ui and added a bit api functionality

--- src/apiFunctionality.js
+++ src/apiFunctionality.js
@@ -1,7 +1,45 @@
 const apiKey = '3d3fc5f176984a28b6121756232808';
-const apiUrl = 'https://api.weatherapi.com/v1/timezone.json?key=3d3fc5f176984a28b6121756232808&q=';
+const submitBtn = document.querySelector('.submit-btn');
+const condition = document.querySelector('.condition');
+const conditionImg = document.querySelector('.condition-img');
+const placeName = document.querySelector('.place-name');
 
-fetch('https://api.weatherapi.com/v1/current.json?key=3d3fc5f176984a28b6121756232808&q=new%20delhi', {mode: 'cors'})
-    .then(function(response) {
-      console.log(response.json());
-    });
+
+export function searchLocation(){
+
+    submitBtn.addEventListener('click', function(){
+        const searchLocation = document.querySelector('#search-location');
+        let searchValue = searchLocation.value;
+        const apiUrl = 'https://api.weatherapi.com/v1/current.json?key=3d3fc5f176984a28b6121756232808&q=' + searchValue;
+        console.log(searchValue);
+        console.log(apiUrl)
+        searchLocation.value = '';
+        fetch(apiUrl, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            condition.textContent = response.current.condition.text;
+            conditionImg.src = response.current.condition.icon;
+            placeName.textContent = response.location.name + ', ' + response.location.country;
+
+
+
+
+
+
+
+
+            console.log(response.location.name);
+            console.log(response.location.country);
+            console.log(response.location.lat);
+            console.log(response.location.lon);
+            console.log(response.location.localtime);
+            console.log(response.current.temp_c);
+            console.log(response.current.temp_f);
+
+
+        });
+        
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-const apiKey = '3d3fc5f176984a28b6121756232808';
-const apiUrl = 'https://api.weatherapi.com/v1/timezone.json?key=3d3fc5f176984a28b6121756232808&q=Delhi';
+import { searchLocation } from "./apiFunctionality";
 
-
-fetch(apiUrl, {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response.location.country);
-    });
+searchLocation();
